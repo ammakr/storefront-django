c0ed1413d58c1bd940e4a9db25bb05f8bb01b66f
AmmarAkramBhutta <aakrambhutta@gmail.com>
2022-08-10 19:37:35 +0500

    'Finish' mosh's course part 1

--- store/models.py
+++ store/models.py
@@ -11,6 +11,12 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -22,6 +28,11 @@ class Product(models.Model):
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
 
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -40,7 +51,12 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
 
+    class Meta:
+        ordering = ['first_name']
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
@@ -57,6 +73,11 @@ class Order(models.Model):
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+    def __str__(self) -> str:
+        return f"{self.customer}'s Order"
+
+    class Meta:
+        ordering = ['customer']
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
