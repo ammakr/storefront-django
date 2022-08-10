4f7a0ac7b49a414f8c4dcc053cda01e70c015d5c
AmmarAkramBhutta <aakrambhutta@gmail.com>
2022-08-10 21:59:47 +0500

    end the day

--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
 
-# Create your views here.
+@api_view()
+def product_list(request):
+    return Response('ok')
+
+@api_view()
+def product_detail(request, id):
+    product = Product.objects.get(pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
