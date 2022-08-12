52d3d4d2f2d503cb3b806b58fbd1407a1e403a3f
AmmarAkramBhutta <aakrambhutta@gmail.com>
2022-08-12 19:40:52 +0500

    Implement product api endpoints

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,8 @@ from . import views
 # URLConf
 urlpatterns = [
     path('products/', views.product_list),
-    path('products/<int:id>/', views.product_detail)
+    path('products/<int:id>/', views.product_detail),
+    path('collection/', views.collection_list),
+    path('collection/<int:id>/', views.collection_detail)
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,18 +1,50 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import ProductSerializer
-from .models import Product
+from .serializers import CollectionSerializer, ProductSerializer
+from .models import Product, Collection
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 
-@api_view()
-def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
+from store import serializers
 
-@api_view()
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'DELETE':
+        product.delete()
+        if product.orderitems.count > 0:
+            return Response({"error": "product can not be deleted because it is associated with an order item."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)        
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST'])
+def collection_list(request):
+    if request.method == 'GET':
+        queryset = Collection.objects.all()
+        serializer = CollectionSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        pass
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def collection_detail(request):
+    return Response('OK')
