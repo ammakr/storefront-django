8aac778d077ff145b35b00ea6cd6976de005963d
AmmarAkramBhutta <aakrambhutta@gmail.com>
2022-08-14 21:14:43 +0500

    implement sorting, pagination, filtering and routers

--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,8 @@ from . import views
 from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
-router.register('products', views.ProductViewSet)
-router.register('collection', views.CollectionViewSet)
+router.register('products', views.ProductViewSet, basename='products')
+router.register('collections', views.CollectionViewSet)
 
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
--- store/views.py
+++ store/views.py
@@ -1,18 +1,28 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from store.pagination import DefaultPagination
+from .filters import ProductFilter
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 from .models import Product, Collection, OrderItem, Review
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
-
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.select_related('collection').all()
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    pagination_class = DefaultPagination
+    filterset_class = ProductFilter
+    search_fields = ['title', 'description' ]
+    ordering_fields = ['unit_price', 'last_update']
 
+    def get_serializer_context(self):
+        return {'request': self.request}
+    
     def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({"error": "product can not be deleted because it is associated with an order item."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
