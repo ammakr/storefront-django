b151c72cee657ecd0ab4363ec0d0f9a9729fd7c7
AmmarAkramBhutta <aakrambhutta@gmail.com>
2022-08-16 18:29:03 +0500

    implement cart API  configuration

--- store/urls.py
+++ store/urls.py
@@ -1,3 +1,4 @@
+from cgitb import lookup
 from django.urls import path
 from . import views
 from rest_framework_nested import routers
@@ -5,9 +6,13 @@ from rest_framework_nested import routers
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
+router.register('carts', views.CartViewSet)
 
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
 
+carts_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+carts_router.register('items', views.CartItemViewSet, basename='cart-items')
+
 # URLConf
-urlpatterns = router.urls + products_router.urls
+urlpatterns = router.urls + products_router.urls + carts_router.urls
--- store/views.py
+++ store/views.py
@@ -2,14 +2,14 @@ from rest_framework.response import Response
 
 from store.pagination import DefaultPagination
 from .filters import ProductFilter
-from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
-from .models import Product, Collection, OrderItem, Review
+from .serializers import CartItemSerializer, CartSerializer, CollectionSerializer, ProductSerializer, ReviewSerializer, AddItemSerializer
+from .models import Product, Collection, OrderItem, Review, Cart, CartItem
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
@@ -49,3 +49,17 @@ class ReviewViewSet(ModelViewSet):
     
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
+
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin,GenericViewSet):
+    queryset = Cart.objects.prefetch_related('items__product').all()
+    serializer_class = CartSerializer
+ 
+class CartItemViewSet(ModelViewSet):
+    
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return AddItemSerializer
+        return CartItemSerializer
+    
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk']).select_related('product')
